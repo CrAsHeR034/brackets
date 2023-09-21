08b925686a7f31fe10464dd90a298eb157f06b73
CrAsHeR034 <128222450+CrAsHeR034@users.noreply.github.com>
2023-09-21 11:04:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const bracketPairs = Object.fromEntries(bracketsConfig);
+
+  for (const char of str) {
+    if (bracketPairs[char]) {
+      // It's an opening bracket, push it onto the stack.
+      stack.push(char);
+    } else {
+      // It's a closing bracket.
+      if (stack.length === 0) {
+        return false; // No corresponding opening bracket.
+      }
+
+      const top = stack.pop();
+      if (bracketPairs[top] !== char) {
+        return false; // Mismatched brackets.
+      }
+    }
+  }
+
+  return stack.length === 0; // Check if the stack is empty.
 }
